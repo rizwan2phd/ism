d3950594e76f9693a4440e0fee70753b462176ee
mrizwan <mrizwan@HP-ENVY-PC>
2017-06-22 20:26:48 +0500

    user edited

--- src/main/java/com/dexdevs/model/CommonEntity.java
+++ src/main/java/com/dexdevs/model/CommonEntity.java
@@ -42,37 +42,5 @@ public abstract class CommonEntity implements Serializable {
     @Version
     private int version;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof CommonEntity)) {
-            return false;
-        }
-        CommonEntity other = (CommonEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.dexdevs.model.CommonEntity[ id=" + id + " ]";
-    }
     
 }
